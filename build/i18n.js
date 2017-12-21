a24ca8b0c14a27a9339154b1c2298f1d7805bdd1
Kentaro Kuribayashi <kentarok@gmail.com>
2017-12-21 20:33:59 +0900

    Remove artifacts in `build/` dir because it will be generated in user's local environment

--- build/i18n.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const i18n = require("i18n");
-i18n.configure({
-    directory: path.join(__dirname + '/../locales')
-});
-exports.default = i18n;
-//# sourceMappingURL=i18n.js.map
